81a274f766a098fa88ef1476c1c08ab98b51f17d
Ayush-cloud-8 <83327864+Ayush-cloud-8@users.noreply.github.com>
2021-06-05 11:59:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -32,6 +32,20 @@ if (item == "") {
 document.getElementById("add_item").value = "";
 }
 
+var with_update = [];
+
+for (i = 0; i < menu_list_array.length; i++) {
+    display_menu =  "<br>" + "<li>" + menu_list_array[i] + "</li>";
+    with_update.push(display_menu);
+    console.log(with_update);
+    console.log(i);
+    console.log(display_menu);
+}
+with_update.sort();
+var space = with_update.join(" ");
+console.log(space);
+document.getElementById("display_menu").innerHTML = space;
+
 
 }
 
